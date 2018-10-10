cdfc35e257e80ce5b730bf28d28bd45919857187
Darkwing42 <jeanmarc.wagner@gmx.de>
2018-10-10 15:07:37 +0200

    Update flask_skel.py

--- flask_skel.py
+++ flask_skel.py
@@ -9,23 +9,33 @@ test_files = ['test/__init__.py']
 main_files = ['run.py', '.gitignore', 'requirements.txt', 'README.md', 'manage.py']
 app_files = ['app/__init__.py', 'app/resources/__init__.py', 'app/models/__init__.py']
 
-def makedirs(list_of_dirs):
-	for each in list_of_dirs:
-		os.mkdir(each)
-def create_files(list_of_files):
-	for each in list_of_files:
-		f = open(each, "w")
-		f.close()
+def create_dir(*lists):
+	for list in lists:
+		print("Using: " + list)
+		for each in list:
+			print("Creating dir: " + each)
+			os.mkdir(each)
+def create_files(*lists):
+	for list in lists:
+		print("Using: " + list)
+		for each in list:
+			print("Creating file: " + each)
+			f = open(each, "w")
+			f.close()
 
 def main():
-	makedirs(app_list)
-	makedirs(config_list)
-	makedirs(test_list)
-
-	create_files(config_files)
-	create_files(main_files)
-	create_files(test_files)
-	create_files(app_files)
+	print("Creating directories")
+	print("########################################################")
+	print("")
+	
+	create_dir(app_list, test_list, config_list)
+	
+	print("")
+	print("Creating files")
+	print("########################################################")
+	print("")
+	create_files(main_files, app_files, test_files, config_files)
+	
 
 
 
