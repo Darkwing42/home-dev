f943e1c8e5ad0550f69ee5cd37c8a6bacfc0cb47
Darkwing42 <jeanmarc.wagner@gmx.de>
2018-10-11 16:24:57 +0200

    Update preferences.py

--- app/models/preferences.py
+++ app/models/preferences.py
@@ -1,1 +1,10 @@
+from app import db
+from app.models.user import User
 
+class Preferences(db.Model):
+	__tablename__ = 'preferences'
+	
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+	weather_api_key = db.Column(db.String(50))
+	
