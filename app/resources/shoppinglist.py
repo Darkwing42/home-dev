290ff2b7ce3a139e0ee5b7e07977e494a429fae7
Darkwing42 <jeanmarc.wagner@gmx.de>
2018-10-11 13:59:54 +0200

    Update shoppinglist.py

--- app/resources/shoppinglist.py
+++ app/resources/shoppinglist.py
@@ -1,1 +1,14 @@
+from flask_restful import Resource, reqparser
+from datetime import datetime
 
+
+class ShoppingList(Resource):
+	def get(self):
+		pass
+	
+	def post(self):
+		pass
+	
+	def put(self):
+		pass
+	
