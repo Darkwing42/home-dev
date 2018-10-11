36b3cb02ed6763dfac3970ae7c30b4d01fbca15a
Darkwing42 <jeanmarc.wagner@gmx.de>
2018-10-11 15:26:56 +0200

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,7 @@ from app import db
 from datetime import datetime
 from app.models.shoppinglist import ShoppingList
 
-class User(db):
+class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
