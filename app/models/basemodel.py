9ab42906eaf660887582606322be7a4c37df4f33
Darkwing42 <jeanmarc.wagner@gmx.de>
2018-10-11 13:58:02 +0200

    Update basemodel.py

--- app/models/basemodel.py
+++ app/models/basemodel.py
@@ -1,1 +1,21 @@
+from app import db
+from datetime import datetime
 
+class BaseModel(db.Model):
+	
+	def save(self):
+        db.session.add(self)
+        db.session.commit()
+		
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
+	
+	@classmethod
+	def get_all(cls):
+		return cls.query.all()
+	
+	@classmethod
+	def get_by_id(cls, id):
+		return cls.query.find_by(id=id).first()
+	
