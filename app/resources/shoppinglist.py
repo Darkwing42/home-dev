db32d51105066c2c32620f4663412ce77cc8f0ff
Darkwing42 <jeanmarc.wagner@gmx.de>
2018-10-11 16:30:22 +0200

    Update shoppinglist.py

--- app/resources/shoppinglist.py
+++ app/resources/shoppinglist.py
@@ -6,11 +6,22 @@ from app.models.user import User
 
 class ShoppingLists(Resource):
 	def get(self):
-		pass
+		shoplists = ShoppingList.get_all()
+		return {'shoppinglists': [ shoplist.to_dict() for shoplist in shoplists ]}, 201
 	
 class ShoppingList(Resource):
 	def get(self, id):
 		pass
+	
+	def post(self):
+		pass
+	
+	def put(self, id):
+		pass
+	
+	def delete(self):
+		pass
+	
 	
 
 	
