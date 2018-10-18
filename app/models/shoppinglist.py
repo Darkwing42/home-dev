05151c8bac9112953be4cc9a11a2b49f1dd83cff
Jean-Marc <jeanmarc.wagner@gmx.de>
2018-10-18 13:44:13 +0200

    änderungen

--- app/models/shoppinglist.py
+++ app/models/shoppinglist.py
@@ -1,1 +1,51 @@
+from app import db
+
+class ShopList(db.Model):
+    
+    __tablename__ = 'shoplists'
+
+    shoplistID = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    items = db.relationship('Item', backref='ShopList', lazy=True)
+
+    def to_dict(self):
+        return dict(
+                shoplistID=self.shoplistID,
+                title=self.title,
+                items=[item.to_dict() for item in self.items]
+                )
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    itemID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    list_id = db.Column(db.Integer, db.ForeignKey('shoplists.shoplistID'))
+
+    def to_dict(self):
+        return dict(
+                itemID=self.itemID,
+                name=self.name,
+                list_id=self.list_id)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+
+
 
--- app/resources/shoppinglist.py
+++ app/resources/shoppinglist.py
@@ -1,44 +1,32 @@
 from flask_restful import Resource
 from flask import request
 from datetime import datetime
-from app.models.shoppinglist import ShoppingList, Item
-from app.models.user import User
+from app.models.shoppinglist import ShopList, Item
 
 
 class ShoppingLists(Resource):
-	def get(self):
-		shoplists = ShoppingList.get_all()
-		return {'shoppinglists': [ shoplist.to_dict() for shoplist in shoplists ]}, 201
+    def get(self):
+        shoplists = ShopList.get_all()
+        return {'shoppinglists': [ shoplist.to_dict() for shoplist in shoplists ]}, 201
 
 class ShoppingList(Resource):
-	def get(self, id):
-		# TODO: get shopping list with specific id
-		pass
 
-	def post(self):
-		data = request.get_json()
-		print(data)
+    def post(self):
+          data = request.get_json()
+          print("")
+          print(data)
+          items = []
+          for item in data['items']:
+              obj = Item(
+                      name=item['name']
+                      )
+              items.append(obj)
 
+          shopList = ShopList(
+                  title=data['title'],
+                  items=items
+                  )
 
-		shoppingItems = []
+          shopList.save()
+          return {'message': 'Liste angelegt'}, 201
 
-		for item in data['items']:
-			obj = Item(
-			name=item['name'],
-			quantity=item['quantity']
-			)
-			shoppingItems.append(obj)
-
-		shopList = ShoppingList(
-			title=data['title'],
-			done=data['done']
-		)
-		shopList.save()
-		return {'message': 'Liste angelegt'}, 201
-
-
-	def put(self, id):
-		pass
-
-	def delete(self, id):
-		pass
